6feb5d790ae48b11cfb22bcb5571e703db84b91d
Baptiste Marcon <baptistemcn@icloud.com>
2021-06-04 08:19:08 +0200

    feat: Tool Bar v2 & Dark Mode

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -11,6 +11,8 @@ import CoreData
 struct ContentView: View {
     
     // MARK: - PROPERTIES
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
+    
     @State var task: String = ""
     @State private var showNewtaskItem: Bool = false
     
@@ -44,6 +46,36 @@ struct ContentView: View {
                 // MARK: - MAIN VIEW
                 VStack {
                     // MARK: - HEADER
+                    HStack(spacing: 10) {
+                        // TITLE
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                    
+                        Spacer()
+                        
+                        // EDIT BUTTON
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(Capsule().stroke(Color.white, lineWidth: 2))
+                        
+                        // APPERANCE BUTTON
+                        Button(action: {
+                            // TOOGLE APPERANCE
+                            isDarkMode.toggle()
+                        }, label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        })
+                        
+                    } //: HSTACK
+                    .padding()
+                    .foregroundColor(.white)
                     Spacer(minLength: 80)
                     
                     // MARK: - NEW TASK BUTTON
@@ -100,13 +132,7 @@ struct ContentView: View {
                 UITableView.appearance().backgroundColor = UIColor.clear
             }
             .navigationBarTitle("Daily Task", displayMode: .large)
-            .toolbar {
-                #if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                #endif
-            } //: TOOLBAR
+            .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
             )
