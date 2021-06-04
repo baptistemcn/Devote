44ef9f973222a9933881523b8600e1880ec659df
Baptiste Marcon <baptistemcn@icloud.com>
2021-06-04 22:50:44 +0200

    feat: Widget Layout v1

--- DevoteWidget/DevoteWidget.swift
+++ DevoteWidget/DevoteWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -40,22 +40,57 @@ struct SimpleEntry: TimelineEntry {
 
 struct DevoteWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        //        Text(entry.date, style: .time)
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: 36, height: 36)
+                    .offset(
+                        x: (geometry.size.width / 2) - 20,
+                        y: (geometry.size.height / -2) + 20
+                    )
+                    .padding(.top, 12)
+                    .padding(.trailing, 12)
+                
+                HStack {
+                    Text("Just do it")
+                        .foregroundColor(.white)
+                        .font(.system(.footnote, design: .rounded))
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(
+                            Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+                                .blendMode(.overlay)
+                        )
+                        .clipShape(Capsule())
+                } //: HSTACK
+                .padding()
+                .offset(y: (geometry.size.height / 2) - 24)
+            } //: ZSTACK
+        } //: GEOMETRY
     }
 }
 
 @main
 struct DevoteWidget: Widget {
     let kind: String = "DevoteWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             DevoteWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Devote Launcher")
+        .description("This is an example widget for the personnal task manager.")
     }
 }
 
